ce004767d1b474b109798479aae51815ebe7caa6
icepy <xiangwenwe@foxmail.com>
2021-12-28 22:07:17 +0800

    chore: 更新合约

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,84 @@
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import './App.css';
+import abi from './utils/WavePortal.json';
 
 export default function App() {
 
   const [currentAccount, setCurrentAccount] = useState("");
+  const [allWaves, setAllWaves] = useState([]);
+  const contractAddress = '0xa8cd2882c1445b0412eA9c966ff9c9Db179E165B';
+  const contractABI = abi.abi;
+  const wave = async() => {
+    try{
+      const { ethereum } = window;
 
-  const wave = () => {
-    
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+         /*
+        * Execute the actual wave from your smart contract
+        */
+         const waveTxn = await wavePortalContract.wave('icepy');
+         console.log("Mining...", waveTxn.hash);
+ 
+         await waveTxn.wait();
+         console.log("Mined -- ", waveTxn.hash);
+ 
+         count = await wavePortalContract.getTotalWaves();
+         console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    }catch(error){
+      console.log(error);
+    }
+  }
+
+  /*
+   * Create a method that gets all waves from your contract
+   */
+  const getAllWaves = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        /*
+         * Call the getAllWaves method from your Smart Contract
+         */
+        const waves = await wavePortalContract.getAllWaves();
+
+
+        /*
+         * We only need address, timestamp, and message in our UI so let's
+         * pick those out
+         */
+        let wavesCleaned = [];
+        waves.forEach(wave => {
+          wavesCleaned.push({
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message
+          });
+        });
+
+        /*
+         * Store our data in React State
+         */
+        setAllWaves(wavesCleaned);
+      } else {
+        console.log("Ethereum object doesn't exist!")
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   const checkIfWalletIsConnected = async() => {
@@ -71,28 +142,34 @@ export default function App() {
   
   return (
     <div className="mainContainer">
-      <div className="dataContainer">
-        <div className="header">
+    <div className="dataContainer">
+      <div className="header">
         👋 Hey there!
-        </div>
-
-        <div className="bio">
-          I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
-        </div>
-
-        <button className="waveButton" onClick={null}>
-          Wave at Me
-        </button>
-
-        {/*
-        * If there is no currentAccount render this button
-        */}
-        {!currentAccount && (
-          <button className="waveButton" onClick={connectWallet}>
-            Connect Wallet
-          </button>
-        )}
       </div>
+
+      <div className="bio">
+        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+      </div>
+
+      <button className="waveButton" onClick={wave}>
+        Wave at Me
+      </button>
+
+      {!currentAccount && (
+        <button className="waveButton" onClick={connectWallet}>
+          Connect Wallet
+        </button>
+      )}
+
+      {allWaves.map((wave, index) => {
+        return (
+          <div key={index} style={{ backgroundColor: "OldLace", marginTop: "16px", padding: "8px" }}>
+            <div>Address: {wave.address}</div>
+            <div>Time: {wave.timestamp.toString()}</div>
+            <div>Message: {wave.message}</div>
+          </div>)
+      })}
     </div>
+  </div>
   );
 }
--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 // 0x5FbDB2315678afecb367f032d93F642f64180aa3 localhost
 // 0x806765197e2A0685f0Fc88DB464119d458B93Ff4 rinkeby
+// 0xa8cd2882c1445b0412eA9c966ff9c9Db179E165B rinkeby 2
 
 pragma solidity ^0.8.0;
 
@@ -8,13 +9,32 @@ import "hardhat/console.sol";
 
 contract WavePortal{
   uint256 totalWaves;
-  constructor(){
-    console.log("Yo yo, I am a contract and I am smart");
+
+  event NewWave(address indexed from, uint256 timestamp, string message);
+
+  struct Wave{
+    address waver;
+    string message;
+    uint256 timestamp;
   }
 
-  function wave() public{
+  Wave[] waves;
+
+  constructor(){
+    console.log("I AM SMART CONTRACT. POG.");
+  }
+
+  function wave(string memory _message) public{
     totalWaves += 1;
-    console.log("%s has waved", msg.sender);
+    console.log("%s waved w/ message %s", msg.sender, _message);
+
+    waves.push(Wave(msg.sender, _message, block.timestamp));
+
+    emit NewWave(msg.sender, block.timestamp, _message);
+  }
+
+  function getAllWaves() public view returns (Wave[] memory) {
+    return waves;
   }
 
   function getTotalWaves() public view returns (uint256){
